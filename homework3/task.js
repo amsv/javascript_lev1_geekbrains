3d351d33f90f4c92b050e404d4a2173abaaa54a2
amsv <ammosovaleksey@ya.ru>
2019-04-29 02:44:30 +0300

    +ДЗ№3

---
+++ homework3/task.js
@@ -0,0 +1,51 @@
+// Задание 1
+console.log(" ");
+ let a = [
+    [{name: 'Mary', age: 23}, {name: 'Mike', age: 45}, {name: 'Nick', age: 11}],
+    [{name: 'Adam', age: 56}, {name: 'Sara', age: 21}, {name: 'Don', age: 22}],
+    [{name: 'Karl', age: 34}, {name: 'Marta', age: 76}, {name: 'John', age: 19}]
+   ];
+ 
+let b = [
+   [{name: '', age: 0}, {name: '', age: 0}, {name: '', age: 0}],
+   [{name: '', age: 0}, {name: '', age: 0}, {name: '', age: 0}],
+   [{name: '', age: 0}, {name: '', age: 0}, {name: '', age: 0}]
+   ];
+
+for (let i = 0; i < a.length; i++) {
+   for (let j = 0; j < a[i].length; j++) {
+      b[i][j].age = a[i][j].age;
+      b[i][j].name = a[i][j].name;  
+   } 
+}
+
+// Задание 2
+// price - цена за ед. товара, amount - кол-во товаров
+console.log("Задание 2");
+let goods = [{name:'Мышь',price: 150 , amount:2},
+            {name:'Клавиатура',price: 400 , amount:2},
+            {name:'Колонки',price: 600 , amount:1},
+            {name:'Микрофон',price: 500 , amount:3}];
+
+function countBasketPrice(x) {
+   let sum = 0;
+   for (let i = 0; i < x.length; i++) {
+            sum+=x[i].price*x[i].amount;
+   }
+   return sum;
+}
+console.log(countBasketPrice(goods));
+
+// Задание 3
+console.log("Задание 3");
+for (let i = 0; i <= 9 ;console.log(i++)) {
+}
+
+// Задание 4
+console.log("Задание 4");
+let star="";
+for (let i = 1; i <= 20 ;i++) {
+   console.log(star+="*");
+}
+
+
